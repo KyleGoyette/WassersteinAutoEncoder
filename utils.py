c7643e516f9d1d548ea5e2c03f2129d61b27ba5b
KyleGoyette <kdgoyette@gmail.com>
2018-04-29 22:59:33 -0400

    fix encoding on pickling

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import torch
 import torch.nn
 import torch.nn.functional as F
@@ -12,23 +14,22 @@ def train(model, dloader, loss_type='VAE'):
 
 def load_data_mnist(batch_size=config.batch_size, test=False):
     train_path = '/data/lisa/data/mnist/mnist-python/train.pkl'
-    if not os.path.isfile(path):
+    if not os.path.isfile(train_path):
         raise Exception('Incorrect path to data')
 
     else:
-        train_data = pickle.load(open(train_path,'rb'))
-        train_data = np.array(train_data)
         
-        train_data.shuffle()
+        train_data = pickle.load(open(train_path,'rb'),encoding='latin1')
+        train_data = np.array(train_data)
 
         train_dataset = torch.utils.data.TensorDataset(torch.Tensor(train_data))
         trainloader = torch.utils.data.Dataloader(train_dataset,batch_size=batch_size,shuffle=True)
         if (test):
-            test_data = pickle.load(open(test_path,'rb'))
+            test_data = pickle.load(open(test_path,'rb'), encoding='latin1')
             test_data = np.array(test_data)
-            test_data.shuffle()
+
             test_dataset = torch.utils.data.TensorDataset(torch.Tensor(test_data))
             testloader = torch.utils.data.Dataloader(test_dataset,batch_size=batch_size,shuffle=True)
             return trainloader, testloader
 
-        return trainloader
+        return trainloader, _
