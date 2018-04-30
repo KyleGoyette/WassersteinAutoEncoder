0c5037ca2555dfd011b950e710c50068c6cbd51d
KyleGoyette <kdgoyette@gmail.com>
2018-04-29 22:40:11 -0400

    fix utils loading

--- utils.py
+++ utils.py
@@ -10,23 +10,23 @@ def train(model, dloader, loss_type='VAE'):
 
         data = Variable(data).cuda()
 
-def load_data_mnist(batch_size=config.batch_size,test_split=0.2):
-    path = '/data/lisa/data/mnist/mnist.pkl'
-    if not os.path.isfile(data):
+def load_data_mnist(batch_size=config.batch_size, test=False):
+    train_path = '/data/lisa/data/mnist/mnist-python/train.pkl'
+    if not os.path.isfile(path):
         raise Exception('Incorrect path to data')
 
     else:
-        data = pickle.load(open(path),'rb')
-        data = np.array(data)
+        train_data = pickle.load(open(train_path,'rb'))
+        train_data = np.array(train_data)
         
-        n = data.shape[0]
-        data.shuffle()
-        train_data = data[:n*int(1-test_split)]
-        
+        train_data.shuffle()
+
         train_dataset = torch.utils.data.TensorDataset(torch.Tensor(train_data))
         trainloader = torch.utils.data.Dataloader(train_dataset,batch_size=batch_size,shuffle=True)
-        if (test_split != 0)
-            test_data = data[n*int(1-test_split):]
+        if (test):
+            test_data = pickle.load(open(test_path,'rb'))
+            test_data = np.array(test_data)
+            test_data.shuffle()
             test_dataset = torch.utils.data.TensorDataset(torch.Tensor(test_data))
             testloader = torch.utils.data.Dataloader(test_dataset,batch_size=batch_size,shuffle=True)
             return trainloader, testloader
