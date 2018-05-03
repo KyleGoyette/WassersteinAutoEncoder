e435d3d3d37a44fec69da8c783a2f9e2fc5d4f0e
KyleGoyette <kdgoyette@gmail.com>
2018-05-03 14:28:49 -0400

    fix learning rates

--- utils.py
+++ utils.py
@@ -118,7 +118,7 @@ def test(model,dloader,epoch,confs,add_noise=True):
         elif confs['loss'] == 'wae-mmd':
             mu, logvar = model.encode(data)
             z_tilde = model.reparameterize(mu,logvar)
-            z = torch.autograd.Variable(torch.cuda.FloatTensor(logvar.shape).normal_())
+            z = torch.autograd.Variable(torch.normal(torch.zeros(config.batch_size,confs['latentd']),std=1.0).cuda())
             recon_x = model.decode(z_tilde)
 
             loss = model.loss(recon_x,data,z,z_tilde)
