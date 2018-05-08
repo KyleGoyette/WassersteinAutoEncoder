dfd22b6aff163ad9b9643630ef86c9e2f9ef1e42
KyleGoyette <kdgoyette@gmail.com>
2018-05-07 20:30:38 -0400

    fix latent traversal

--- latent_traversal.py
+++ latent_traversal.py
@@ -7,16 +7,18 @@ import WAE
 import VAE
 import os
 
-def traverse_latent(model,num_images=10):
-    z1 = torch.autograd.Variable(torch.normal(torch.zeros(num_images,model.confs['latentd']),std=1)).cuda() 
-    z2 = torch.autograd.Variable(torch.normal(torch.zeros(num_images,model.confs['latentd']),std=1)).cuda() 
+def traverse_latent(model,images1,images2,num_images=10):
+    mu1,logvar1 = model.encode(images1)
+    mu2,logvar2 = model.encode(images2) 
+    z1 = model.reparameterize(mu1,logvar1)
+    z2 = model.reparameterize(mu2,logvar2)
 
     alphas = [0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
     results = None
 
     for alpha in alphas:
-        z = alpha*z1 + (1-alpha*z2)
-        #z = torch.autograd.Variable(z).cuda()
+        z = alpha*z1 + (1-alpha)*z2
+        
         recon_x = model.decode(z)
         if type(results) == type(None):
            results = recon_x.data
